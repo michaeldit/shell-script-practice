c96ded523af57ab55cd2292050f4447820aedd26
Jay Dawn <drunkenmau@gmail.com>
2022-05-07 17:37:19 -0500

    Added new mySQL scanner script
    Script includes prompts for IP Addresses and port numbers.

---
+++ improved_mySQLscanner.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Enter the starting IP address : "
+read FirstIP
+
+echo "Enter the last octet of the last IP address : "
+read LastOctetIP
+
+echo "Enter the port number you want to scan for: "
+read port
+
+nmap -sT $FirstIP-$LastOctetIP -p $port > /dev/null -oG MySQLscan
+
+cat MySQLscan | grep open > MySQLscan2
+
+cat MySQLscan2
+
+echo "Delete files after reading? y or n?"
+read delete
+
+if $delete == 'y'
+then
+	rm MySQLscan && MySQLscan2
+else
+	break	
+fi
+
+
